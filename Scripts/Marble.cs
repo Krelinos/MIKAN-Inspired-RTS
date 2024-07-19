96f38ee0e7b14faf6a3270ae85b5cb4072cfd4bd
Krelinos <emmer.palaming@gmail.com>
2024-07-19 02:02:25 -0500

    Trail mechanics decoupled into its own class

--- Scripts/Marble.cs
+++ Scripts/Marble.cs
@@ -1,65 +1,20 @@
 using Godot;
 using System;
 
-public class Marble : RigidBody2D
+public class Marble : BaseRTSEntity
 {
-	private Line2D Trail;
-
-	private Godot.Vector2[] TrailPointsGlobal;
+	private Trail Trail;
 
 	public override void _Ready()
 	{
-		Trail = GetNode<Line2D>("Trail");
+		Trail = GetNode<Trail>("Trail");
 
-		var TrailColor = GetNode<Polygon2D>("AntialiasedRegularPolygon2D").Color;
-		var TrailOutline = (Color)GetNode("AntialiasedRegularPolygon2D").Get("stroke_color");
-		// Ignore the error. AntialiasedRegularPolygon2D is from a GDScript and VSCode cannot detect it.
+		var MarbleColor = GetNode<Polygon2D>("AntialiasedRegularPolygon2D").Color;
+		var MarbleOutline = (Color)GetNode("AntialiasedRegularPolygon2D").Get("stroke_color");
 		var TrailSize = (Godot.Vector2)GetNode("AntialiasedRegularPolygon2D").Get("size");
 
-		Trail.Gradient.Colors = new Color[]{ TrailOutline, TrailColor * new Color(1, 1, 1, 0) };
+		Trail.SetStartColor( MarbleOutline );
+		Trail.SetEndColor( MarbleColor * new Color(1, 1, 1, 0) );
 		Trail.Width = Math.Min( TrailSize.x, TrailSize.y );
-
-		TrailPointsGlobal = new Godot.Vector2[ Trail.Points.Length ];
-	}
-
-	//  // Called every frame. 'delta' is the elapsed time since the previous frame.
-	//  public override void _Process(float delta)
-	//  {
-	//      
-	//  }
-
-	public override void _PhysicsProcess(float delta)
-	{
-		base._PhysicsProcess(delta);
-
-		// var _velocity = Velocity;
-		// _velocity += new Vector2( 0, Gravity );
-
-		// Velocity = _velocity;
-
-		// MoveAndSlide( Velocity * delta );
-
-		Update();
-	}
-
-	public override void _Draw()
-	{
-		base._Draw();
-
-		// Right shift the trail array so the older positions are closer to the tail end.
-		var Len = TrailPointsGlobal.Length;
-		var RShiftedTrail = new Godot.Vector2[ Len ];
-		for ( int i = 1; i < Len; i++ )
-			RShiftedTrail[ i ] = TrailPointsGlobal[ i - 1 ];
-		RShiftedTrail[ 0 ] = GlobalPosition;
-
-		TrailPointsGlobal = RShiftedTrail;
-
-		// Then translate the global positions to the marble's local position
-		var TrailPointsLocal = new Godot.Vector2[ Len ];
-		for( int i = 0; i < Len; i++ )
-			TrailPointsLocal[ i ] = ToLocal( TrailPointsGlobal[ i ] );
-		
-		Trail.Points = TrailPointsLocal;
 	}
 }
---
+++ Scripts/Trail.cs
@@ -0,0 +1,57 @@
+using Godot;
+using System;
+using System.ComponentModel;
+
+public class Trail : Line2D
+{
+    [Export] public int Length { get; private set; } = 16;
+    [Export] public Color StartColor { get; private set; } = new Color(1,1,1);
+    [Export] public Color EndColor { get; private set; } = new Color(0.8f,0.8f,0.8f);
+
+    private Vector2[] PointsGlobal;
+
+    private float TimeSinceLastUpdate = 0f;
+    private const float UpdateIncrementTime = 1/30f;
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        Points = new Vector2[ Length ];
+        PointsGlobal = new Vector2[ Length ];
+        for( int i = 0; i < Length; i++ )
+            PointsGlobal[i] = GlobalPosition;
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(float delta)
+    {
+        TimeSinceLastUpdate += delta;
+
+        if ( TimeSinceLastUpdate >= UpdateIncrementTime )     // Update trail every 1/60 seconds.
+        {
+            UpdatePointPositions();
+            TimeSinceLastUpdate -= UpdateIncrementTime;
+        }
+    }
+
+    private void UpdatePointPositions()
+    {
+        // Right shift the trail array so the older positions are closer to the tail end.
+		var RShiftedTrail = new Godot.Vector2[ Length ];
+		for ( int i = 1; i < Length; i++ )
+			RShiftedTrail[ i ] = PointsGlobal[ i - 1 ];
+		RShiftedTrail[ 0 ] = GlobalPosition;
+
+		PointsGlobal = RShiftedTrail;
+
+		// Then translate the global positions to the trail's local position
+		var TrailPointsLocal = new Godot.Vector2[ Length ];
+		for( int i = 0; i < Length; i++ )
+			TrailPointsLocal[ i ] = ToLocal( PointsGlobal[ i ] );
+		
+		Points = TrailPointsLocal;
+    }
+
+    public void SetStartColor( Color val ) { StartColor = val; Gradient.Colors = new Color[] { StartColor, EndColor }; }
+    public void SetEndColor( Color val ) { EndColor = val; Gradient.Colors = new Color[] { StartColor, EndColor }; }   
+}
